855ca761258bcaec5fc37ac09c6a47d7085dac64
hheydarian <hamdhydryan12@gmail.com>
2025-08-05 15:45:50 +0330

    add links ...

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,29 +2,31 @@ import { defineConfig } from "vitepress";
 
 export default defineConfig({
     lang: "fa-IR",
-    title: "کتابخانه من",
+    title: "farsiBooks",
     description: "مستندات ترجمه‌های فارسی",
     dir: "rtl",
     themeConfig: {
         logo: "/logo.png",
     },
-    // head: [
-    //     [
-    //         "link",
-    //         {
-    //             rel: "stylesheet",
-    //             href: "https://cdn.fontcdn.ir/Font/Persian/Shabnam/Shabnam.css",
-    //         },
-    //     ],
-    //     [
-    //         "style",
-    //         {},
-    //         `
-    //   body {
-    //     font-family: 'Shabnam', sans-serif;
-    //   }
-    // `,
-    //     ],
-    // ],
+    sidebar: {
+        "/books/csharp-12/": [
+            {
+                text: "C# 12 in a Nutshell",
+                items: [
+                    { text: "مقدمه", link: "/books/csharp-12/preface" },
+                    { text: "فصل اول", link: "/books/csharp-12/chapter1" },
+                ],
+            },
+        ],
+        "/books/clean-code/": [
+            {
+                text: "Clean Code in C#",
+                items: [
+                    { text: "مقدمه", link: "/books/clean-code/preface" },
+                    { text: "فصل اول", link: "/books/clean-code/chapter1" },
+                ],
+            },
+        ],
+    },
     head: [["link", { rel: "stylesheet", href: "/fonts/shabnam.css" }]],
 });
