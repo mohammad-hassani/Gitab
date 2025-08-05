34cfd4c93de11fac6d70b048975c66812a77288d
hheydarian <hamdhydryan12@gmail.com>
2025-08-05 23:54:47 +0330

    ...

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,31 +2,11 @@ import { defineConfig } from "vitepress";
 
 export default defineConfig({
     lang: "fa-IR",
-    title: "farsiBooks",
+    title: "parsBook",
     description: "مستندات ترجمه‌های فارسی",
     dir: "rtl",
     themeConfig: {
         logo: "/logo.png",
-    },
-    sidebar: {
-        "/books/csharp-12/": [
-            {
-                text: "C# 12 in a Nutshell",
-                items: [
-                    { text: "مقدمه", link: "/books/csharp-12/preface" },
-                    { text: "فصل اول", link: "/books/csharp-12/chapter1" },
-                ],
-            },
-        ],
-        "/books/clean-code/": [
-            {
-                text: "Clean Code in C#",
-                items: [
-                    { text: "مقدمه", link: "/books/clean-code/preface" },
-                    { text: "فصل اول", link: "/books/clean-code/chapter1" },
-                ],
-            },
-        ],
     },
     head: [["link", { rel: "stylesheet", href: "/fonts/shabnam.css" }]],
 });
