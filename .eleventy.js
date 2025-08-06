91cc7b5f5bd9625314ae87bd087323514fcdfef5
hheydarian <hamdhydryan12@gmail.com>
2025-08-07 00:19:23 +0330

    create template customs

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (eleventyConfig) {
+    // کپی استایل‌ها به فولدر خروجی
+    eleventyConfig.addPassthroughCopy("src/styles");
+    eleventyConfig.addPassthroughCopy("src/images");
+
+    return {
+        dir: {
+            input: "src", // ورودی همه فایل‌ها از این پوشه
+            output: "_site", // خروجی سایت
+            includes: "includes", // قالب‌ها
+        },
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+    };
+};
