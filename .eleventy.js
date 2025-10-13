ce79b764bc1f776e34f3f31b7968dc6589fd2be1
hheydarian <hamdhydryan12@gmail.com>
2025-10-13 23:15:48 +0330

    Edit page chapters

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,50 @@
+const markdownIt = require("markdown-it");
+const hljs = require("highlight.js");
+
 module.exports = function (eleventyConfig) {
-    // کپی استایل‌ها به فولدر خروجی
+    /* --- Passthrough: فایل‌هایی که باید کپی بشن --- */
     eleventyConfig.addPassthroughCopy("src/styles");
+    eleventyConfig.addPassthroughCopy("src/fonts");
+
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/books/book1/assets");
     eleventyConfig.addPassthroughCopy("src/books/book2/assets");
-    
     eleventyConfig.addPassthroughCopy("src/icone");
-    eleventyConfig.addGlobalData("layout", "layout.njk");
+
+    /* --- Markdown با Highlight.js --- */
+    const markdownLib = markdownIt({
+        html: true,
+        breaks: true,
+        linkify: true,
+        highlight: function (str, lang) {
+            if (lang && hljs.getLanguage(lang)) {
+                try {
+                    return (
+                        '<pre class="hljs"><code>' +
+                        hljs.highlight(str, {
+                            language: lang,
+                            ignoreIllegals: true,
+                        }).value +
+                        "</code></pre>"
+                    );
+                } catch (__) {}
+            }
+            return (
+                '<pre class="hljs"><code>' +
+                markdownIt().utils.escapeHtml(str) +
+                "</code></pre>"
+            );
+        },
+    });
+    eleventyConfig.setLibrary("md", markdownLib);
+
+    /* --- مسیرهای پروژه --- */
     return {
         dir: {
-            input: "src", // ورودی همه فایل‌ها از این پوشه
+            input: "src", // مسیر اصلی پروژه
             output: "_site", // خروجی سایت
-            includes: "includes", // قالب‌ها
+            includes: "_includes", // مسیر قالب‌ها (حتماً با آندرلاین)
+            layouts: "_includes",
         },
         markdownTemplateEngine: "njk",
         htmlTemplateEngine: "njk",
